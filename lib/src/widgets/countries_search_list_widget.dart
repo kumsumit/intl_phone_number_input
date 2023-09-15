fded93c04766c156fd9580b4a9c98adec75fd945
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-09-15 18:06:14 +0530

    dart fix

--- lib/src/widgets/countries_search_list_widget.dart
+++ lib/src/widgets/countries_search_list_widget.dart
@@ -152,7 +152,7 @@ class _Flag extends StatelessWidget {
         ? SizedBox(
             child: Text(
             Utils.generateFlagEmojiUnicode(country?.alpha2Code ?? ''),
-            style: Theme.of(context).textTheme.headline5,
+            style: Theme.of(context).textTheme.headlineSmall,
           ))
         : SizedBox.shrink();
   }
