9c1cf82eaa1050e6fb20709c53602e5b93492660
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-10-03 11:18:11 +0530

    exported EnumParser extension

--- lib/intl_phone_number_input.dart
+++ lib/intl_phone_number_input.dart
@@ -3,5 +3,6 @@ library intl_phone_number_input;
 export 'src/utils/phone_number.dart';
 export 'src/widgets/input_widget.dart';
 export 'src/utils/selector_config.dart';
+export 'src/utils/util.dart' show EnumParser;
 export 'package:phone_numbers_parser/phone_numbers_parser.dart'
     show PhoneNumber, PhoneNumberType, IsoCode;
