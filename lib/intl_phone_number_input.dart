db0545fd5285f56aed1102af7e870027ca1852f6
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-10-08 10:42:26 +0530

    files added for export

--- lib/intl_phone_number_input.dart
+++ lib/intl_phone_number_input.dart
@@ -4,5 +4,7 @@ export 'src/utils/phone_number.dart';
 export 'src/widgets/input_widget.dart';
 export 'src/utils/selector_config.dart';
 export 'src/utils/util.dart' show EnumParser;
+export 'src/models/country_list.dart';
+export 'src/models/country_model.dart';
 export 'package:phone_numbers_parser/phone_numbers_parser.dart'
     show PhoneNumber, PhoneNumberType, IsoCode;
